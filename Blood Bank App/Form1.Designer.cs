38c26a4e9bd339453d208ea211c581ddf04dca7b
saad abdeen <saad.abdeen@yahoo.com>
2019-05-24 12:30:59 +0500

    C# Blood Bank App

---
+++ Blood Bank App/Form1.Designer.cs
@@ -0,0 +1,363 @@
+﻿namespace Blood_Bank_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.donorNumber = new System.Windows.Forms.TextBox();
+            this.donorName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtWeight = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.donorBloodGrp = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.donorAddress = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.donorContact = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.dashboardInfo = new System.Windows.Forms.DataGridView();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.listSearch = new System.Windows.Forms.TextBox();
+            this.txtgender = new System.Windows.Forms.TextBox();
+            this.donordateofdonation = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dashboardInfo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(204)))), ((int)(((byte)(40)))));
+            this.label1.Location = new System.Drawing.Point(345, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(262, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Blood Bank App";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(48, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(228, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Donor Registration Number";
+            // 
+            // donorNumber
+            // 
+            this.donorNumber.Location = new System.Drawing.Point(282, 73);
+            this.donorNumber.Name = "donorNumber";
+            this.donorNumber.Size = new System.Drawing.Size(163, 20);
+            this.donorNumber.TabIndex = 2;
+            // 
+            // donorName
+            // 
+            this.donorName.Location = new System.Drawing.Point(282, 109);
+            this.donorName.Name = "donorName";
+            this.donorName.Size = new System.Drawing.Size(163, 20);
+            this.donorName.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(47, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Donor Name";
+            // 
+            // txtWeight
+            // 
+            this.txtWeight.Location = new System.Drawing.Point(282, 145);
+            this.txtWeight.Name = "txtWeight";
+            this.txtWeight.Size = new System.Drawing.Size(100, 20);
+            this.txtWeight.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.Location = new System.Drawing.Point(52, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Weight (Kg)";
+            // 
+            // donorBloodGrp
+            // 
+            this.donorBloodGrp.Location = new System.Drawing.Point(704, 182);
+            this.donorBloodGrp.Name = "donorBloodGrp";
+            this.donorBloodGrp.Size = new System.Drawing.Size(100, 20);
+            this.donorBloodGrp.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label5.Location = new System.Drawing.Point(588, 182);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(110, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Blood Group";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label6.Location = new System.Drawing.Point(588, 221);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Gender";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label7.Location = new System.Drawing.Point(504, 298);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(147, 20);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Date of Donation";
+            // 
+            // donorAddress
+            // 
+            this.donorAddress.Location = new System.Drawing.Point(282, 184);
+            this.donorAddress.Multiline = true;
+            this.donorAddress.Name = "donorAddress";
+            this.donorAddress.Size = new System.Drawing.Size(229, 86);
+            this.donorAddress.TabIndex = 10;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label8.Location = new System.Drawing.Point(52, 190);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(75, 20);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Address";
+            // 
+            // donorContact
+            // 
+            this.donorContact.Location = new System.Drawing.Point(282, 298);
+            this.donorContact.Name = "donorContact";
+            this.donorContact.Size = new System.Drawing.Size(136, 20);
+            this.donorContact.TabIndex = 12;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label9.Location = new System.Drawing.Point(52, 298);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(139, 20);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "Contact Number";
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(159)))), ((int)(((byte)(136)))));
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.ForeColor = System.Drawing.Color.White;
+            this.btnSave.Location = new System.Drawing.Point(197, 654);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(101, 41);
+            this.btnSave.TabIndex = 15;
+            this.btnSave.Text = "SAVE";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(132)))), ((int)(((byte)(1)))));
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnDelete.Location = new System.Drawing.Point(432, 654);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(101, 41);
+            this.btnDelete.TabIndex = 15;
+            this.btnDelete.Text = "DELETE";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(65)))), ((int)(((byte)(126)))));
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnReset.Location = new System.Drawing.Point(657, 654);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(101, 41);
+            this.btnReset.TabIndex = 15;
+            this.btnReset.Text = "RESET";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // dashboardInfo
+            // 
+            this.dashboardInfo.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.dashboardInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dashboardInfo.Location = new System.Drawing.Point(51, 393);
+            this.dashboardInfo.Name = "dashboardInfo";
+            this.dashboardInfo.ReadOnly = true;
+            this.dashboardInfo.Size = new System.Drawing.Size(840, 234);
+            this.dashboardInfo.TabIndex = 16;
+            this.dashboardInfo.DoubleClick += new System.EventHandler(this.dashboardInfo_DoubleClick);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(159)))), ((int)(((byte)(136)))));
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.ForeColor = System.Drawing.Color.White;
+            this.btnSearch.Location = new System.Drawing.Point(310, 348);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(122, 28);
+            this.btnSearch.TabIndex = 15;
+            this.btnSearch.Text = "SEARCH";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // listSearch
+            // 
+            this.listSearch.Location = new System.Drawing.Point(51, 354);
+            this.listSearch.Name = "listSearch";
+            this.listSearch.Size = new System.Drawing.Size(250, 20);
+            this.listSearch.TabIndex = 14;
+            // 
+            // txtgender
+            // 
+            this.txtgender.Location = new System.Drawing.Point(704, 221);
+            this.txtgender.Name = "txtgender";
+            this.txtgender.Size = new System.Drawing.Size(100, 20);
+            this.txtgender.TabIndex = 6;
+            // 
+            // donordateofdonation
+            // 
+            this.donordateofdonation.Location = new System.Drawing.Point(657, 298);
+            this.donordateofdonation.Name = "donordateofdonation";
+            this.donordateofdonation.Size = new System.Drawing.Size(151, 20);
+            this.donordateofdonation.TabIndex = 6;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.Yellow;
+            this.label10.Location = new System.Drawing.Point(504, 321);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(204, 24);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "*Enter date as 1-1-1990";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DodgerBlue;
+            this.ClientSize = new System.Drawing.Size(950, 725);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.dashboardInfo);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.listSearch);
+            this.Controls.Add(this.donorContact);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.donorAddress);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.donordateofdonation);
+            this.Controls.Add(this.txtgender);
+            this.Controls.Add(this.donorBloodGrp);
+            this.Controls.Add(this.txtWeight);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.donorName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.donorNumber);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Blood Bank App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dashboardInfo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox donorNumber;
+        private System.Windows.Forms.TextBox donorName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtWeight;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox donorBloodGrp;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox donorAddress;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox donorContact;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.DataGridView dashboardInfo;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox listSearch;
+        private System.Windows.Forms.TextBox txtgender;
+        private System.Windows.Forms.TextBox donordateofdonation;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ Blood Bank App/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Blood_Bank_App
+{
+    public partial class Form1 : Form
+    {
+        int S_No = 0;
+        SqlConnection sqlCon = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\Saad Xeon\Documents\Visual Studio 2013\Projects\Blood Bank App\DB\BloodBank_DB.mdf;Integrated Security=True;Connect Timeout=30");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (sqlCon.State == ConnectionState.Closed)
+                    sqlCon.Open();
+                if (btnSave.Text == "Save")
+                {
+                    SqlCommand sqlCmd = new SqlCommand("BloodbankAddorEdit", sqlCon);
+                    sqlCmd.CommandType = CommandType.StoredProcedure;
+                    sqlCmd.Parameters.AddWithValue("@mode", "Add");
+                    sqlCmd.Parameters.AddWithValue("@S_No", S_No);
+                    sqlCmd.Parameters.AddWithValue("@Donor_Reg_No", donorNumber.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@Donor_Name", donorName.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@Weight", txtWeight.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@Blood_Group", donorBloodGrp.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@Gender", txtgender.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@Date_of_Donation", donordateofdonation.Text);
+                    sqlCmd.Parameters.AddWithValue("@Contact_Number", donorContact.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@Address", donorAddress.Text.Trim());
+                    sqlCmd.ExecuteNonQuery();
+                    MessageBox.Show("Successfully Saved");
+                }
+                    
+                else
+                {
+                    SqlCommand sqlCmd = new SqlCommand("BloodbankAddorEdit", sqlCon);
+                    sqlCmd.CommandType = CommandType.StoredProcedure;
+                    sqlCmd.Parameters.AddWithValue("@mode", "Edit");
+                    sqlCmd.Parameters.AddWithValue("@S_No", S_No);
+                    sqlCmd.Parameters.AddWithValue("@Donor_Reg_No", donorNumber.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@Donor_Name", donorName.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@Weight", txtWeight.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@Blood_Group", donorBloodGrp.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@Gender", txtgender.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@Date_of_Donation", donordateofdonation.Text);
+                    sqlCmd.Parameters.AddWithValue("@Contact_Number", donorContact.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@Address", donorAddress.Text.Trim());
+                    sqlCmd.ExecuteNonQuery();
+                    MessageBox.Show("Successfully Updated");
+
+                }
+                //Reset();
+                FillDataGridView();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message,"Error Message");
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
+        }
+        void FillDataGridView()
+        {
+            if (sqlCon.State == ConnectionState.Closed)
+                sqlCon.Open();
+            SqlDataAdapter SqlDa = new SqlDataAdapter("BloodBankVieworSearch", sqlCon);
+            SqlDa.SelectCommand.CommandType = CommandType.StoredProcedure;
+            SqlDa.SelectCommand.Parameters.AddWithValue("@Blood_Group", listSearch.Text.Trim());
+            DataTable dtbl = new DataTable();
+            SqlDa.Fill(dtbl);
+            dashboardInfo.DataSource = dtbl;
+            dashboardInfo.Columns[0].Visible = false;
+            sqlCon.Close();
+        }
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FillDataGridView();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message,"Error Message");
+            }
+        }
+
+        private void dashboardInfo_DoubleClick(object sender, EventArgs e)
+        {
+            if (dashboardInfo.CurrentRow.Index!= -1)
+            {
+                S_No = Convert.ToInt32(dashboardInfo.CurrentRow.Cells[0].Value.ToString());
+                donorNumber.Text = dashboardInfo.CurrentRow.Cells[1].Value.ToString();
+                donorName.Text = dashboardInfo.CurrentRow.Cells[2].Value.ToString();
+                txtWeight.Text = dashboardInfo.CurrentRow.Cells[3].Value.ToString();
+                donorBloodGrp.Text = dashboardInfo.CurrentRow.Cells[4].Value.ToString();
+                txtgender.Text = dashboardInfo.CurrentRow.Cells[5].Value.ToString();
+                donordateofdonation.Text = dashboardInfo.CurrentRow.Cells[6].Value.ToString();
+                donorContact.Text = dashboardInfo.CurrentRow.Cells[7].Value.ToString();
+                donorAddress.Text = dashboardInfo.CurrentRow.Cells[8].Value.ToString();
+
+                btnSave.Text = "Update";
+                btnDelete.Enabled = true;
+
+            }
+        }
+
+        void Reset()
+        {
+            donorNumber.Text = donorName.Text = txtWeight.Text = donorBloodGrp.Text = txtgender.Text = donordateofdonation.Text = donorContact.Text = donorAddress.Text = "";
+            btnSave.Text = "Save";
+            S_No = 0;
+            btnDelete.Enabled = false;
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Reset();
+            FillDataGridView();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (sqlCon.State == ConnectionState.Closed)
+                    sqlCon.Open();
+                SqlCommand sqlCmd = new SqlCommand("Bloodbankdeletion", sqlCon);
+                sqlCmd.CommandType = CommandType.StoredProcedure;
+                sqlCmd.Parameters.AddWithValue("@S_No", S_No);
+                sqlCmd.ExecuteNonQuery();
+                MessageBox.Show("Successfully Deleted");
+                Reset();
+                FillDataGridView();
+
+            }
+            catch (Exception ex)
+            {
+                
+               MessageBox.Show(ex.Message,"Error Message");
+            }
+        }
+    }
+}
